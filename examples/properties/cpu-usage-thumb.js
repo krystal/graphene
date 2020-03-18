8ee98ff586b364aa3947a203749b04803cf39378
Jonathan Simmonds <js1300@hotmail.co.uk>
2020-03-18 15:41:32 +0000

    Added a thumbnail version of one of the example graphs

---
+++ examples/properties/cpu-usage-thumb.js
@@ -0,0 +1,37 @@
+var CPU_USAGE_THUMB_PROPERTIES = {
+  "colours": {
+      "background": "#FFFFFF",
+      "horizontal_lines": "#F7F7FC",
+      "graph": [
+          "#038FFB",
+          "#04E395",
+          "#FEB01A",
+          "#FF4560"
+      ],
+      "axes_labels": "#888888",
+      "highlight_indicator": "#E0DEFF"
+  },
+  "alphas": {
+      "under_graph": 0
+  },
+  "axes_labels": {
+      "font_family": "Arial",
+      "font_size": 11
+  },
+  "graph_settings": {
+      "line_width": 0.5
+  },
+  "highlight": {
+      "enabled": false,
+      "transistion_duration": 1000
+  },
+  "x_axis": {
+      "range": 288
+  },
+  "y_axis": {
+      "min": 0,
+      "max": 10,
+      "label_interval": 4,
+      "label_suffix": "%"
+  }
+};
