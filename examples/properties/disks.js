797f0efabc7d1799ecf5ae53d6ad2dacb94e8056
Jonathan Simmonds <js1300@hotmail.co.uk>
2020-03-18 13:30:54 +0000

    Added disks to the example

---
+++ examples/properties/disks.js
@@ -0,0 +1,35 @@
+var DISKS_PROPERTIES = {
+  "colours": {
+      "background": "#FFFFFF",
+      "horizontal_lines": "#F7F7FC",
+      "graph": [
+          "#038FFB",
+          "#04E395"
+      ],
+      "axes_labels": "#555555",
+      "highlight_indicator": "#E0DEFF"
+  },
+  "alphas": {
+      "under_graph": 0.1
+  },
+  "axes_labels": {
+      "font_family": "Arial",
+      "font_size": 13
+  },
+  "graph_settings": {
+      "line_width": 1
+  },
+  "highlight": {
+      "enabled": false,
+      "transistion_duration": 1000
+  },
+  "x_axis": {
+      "range": 288
+  },
+  "y_axis": {
+      "min": 0,
+      "max": 250000,
+      "label_interval": 25000,
+      "label_suffix": " b/s"
+  }
+};
