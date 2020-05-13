7132d299df3548a9f5db6351d5427c526f953ed1
Jonathan Simmonds <js1300@hotmail.co.uk>
2020-05-13 09:09:18 +0100

    Moved a function from graph to helper

--- src/graph.js
+++ src/graph.js
@@ -4,24 +4,13 @@ class GrapheneGraph {
       this.foreground = document.getElementById(foregroundId);
       this.canvasWidth = this.background.width;
       this.canvasHeight = this.background.height;
-      this.backgroundContext = this.getContext(this.background);
+      this.backgroundContext = GrapheneHelper.getContext(this.background);
       if (this.foreground) {
-        this.foregroundContext = this.getContext(this.foreground);
+        this.foregroundContext = GrapheneHelper.getContext(this.foreground);
       }
       
       this.properties = properties ? JSON.parse(properties) : null;
       this.data = JSON.parse(data);
       this.draw();
-  }
-
-  getContext(canvas) {
-      var devicePixelRatio = window.devicePixelRatio;
-      canvas.width = this.canvasWidth * devicePixelRatio;
-      canvas.height = this.canvasHeight * devicePixelRatio;
-      canvas.style.width = this.canvasWidth + "px";
-      canvas.style.height = this.canvasHeight + "px";
-      var context = canvas.getContext('2d');
-      context.scale(devicePixelRatio, devicePixelRatio);
-      return context;
   }
 }
