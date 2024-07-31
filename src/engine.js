c13093e37c6c8a4c0267e57eb739e4e0110f7c03
js1300 <js1300@hotmail.co.uk>
2024-07-31 22:42:29 +0100

    Linting

--- src/engine.js
+++ src/engine.js
@@ -1,12 +1,12 @@
-import Linegraph from './linegraph.js';
-import Piegraph from './piegraph.js';
-import Bargraph from './bargraph.js';
+import Bargraph from "./bargraph.js";
+import Linegraph from "./linegraph.js";
+import Piegraph from "./piegraph.js";
 
 export default class Engine {
   constructor() {
-    this.graphDictionary = new Object();
+    this.graphDictionary = {};
 
-    window.addEventListener('resize', this.rerender.bind(this));
+    window.addEventListener("resize", this.rerender.bind(this));
   }
 
   addLinegraph(element, properties, data, axisFormatter, informationFormatter) {
@@ -17,7 +17,7 @@ export default class Engine {
       data = JSON.parse(data);
     }
 
-    var linegraph = new Linegraph(element, properties, data, axisFormatter, informationFormatter);
+    const linegraph = new Linegraph(element, properties, data, axisFormatter, informationFormatter);
 
     this.graphDictionary[element.id] = linegraph;
 
@@ -32,7 +32,7 @@ export default class Engine {
       data = JSON.parse(data);
     }
 
-    var piegraph = new Piegraph(element, properties, data);
+    const piegraph = new Piegraph(element, properties, data);
 
     this.graphDictionary[element.id] = piegraph;
 
@@ -47,7 +47,7 @@ export default class Engine {
       data = JSON.parse(data);
     }
 
-    var bargraph = new Bargraph(element, properties, data, axisFormatter, informationFormatter);
+    const bargraph = new Bargraph(element, properties, data, axisFormatter, informationFormatter);
 
     this.graphDictionary[element.id] = bargraph;
 
@@ -55,8 +55,8 @@ export default class Engine {
   }
 
   render() {
-    for (var elementId in this.graphDictionary) {
-      var graph = this.graphDictionary[elementId];
+    for (const elementId in this.graphDictionary) {
+      const graph = this.graphDictionary[elementId];
       if (!graph.drawn) {
         graph.draw();
       }
@@ -64,8 +64,8 @@ export default class Engine {
   }
 
   rerender() {
-    for (var elementId in this.graphDictionary) {
-      var graph = this.graphDictionary[elementId];
+    for (const elementId in this.graphDictionary) {
+      const graph = this.graphDictionary[elementId];
       graph.createLayers();
       graph.addMouseEvents();
       if (graph.drawn) {
@@ -79,8 +79,8 @@ export default class Engine {
   }
 
   removeGraph(element) {
-    var graph = this.getGraph(element);
+    const graph = this.getGraph(element);
     graph.removeLayers();
-    delete this.graphDictionary[element.id];
+    Reflect.deleteProperty(this.graphDictionary, element.id);
   }
 }
